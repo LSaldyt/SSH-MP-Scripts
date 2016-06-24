39df204bf0dfb279effe9c6bb7ed49fc35b3a3a7
lucas <lucassaldyt@gmail.com>
2016-06-24 16:41:13 -0700

    Prevents users of server from sshing in
    Modifies the shell of the git user to point to git-shell, effectively disabling raw ssh. However, users will still be able to push and pull from the server (which is what is intended)

--- sudo_git_setup.sh
+++ sudo_git_setup.sh
@@ -6,3 +6,19 @@ adduser git
 cd /home/git/
 mkdir .ssh && chmod 700 .ssh
 touch .ssh/authorized_keys && chmod 600 .ssh/authorized_keys
+
+# Install git if it doesn't exist. Assume Ubuntu :)
+hash git 2>/dev/null || { apt-get update; apt-get install git-core; }
+
+#Restrict ssh access to git-shell only
+if [ ! -f /etc/shells ]
+then echo "No Shells file"
+     touch /etc/shells;
+else echo "Shells file found";
+fi
+
+hash git-shell 2>/dev/null || { echo "WARNING: git-shell not found, ssh users will have normal access"; }
+
+hash git-shell 2>/dev/null && { echo "/usr/bin/git-shell" >> /etc/shells; }
+
+chsh git -s /usr/bin/git-shell
