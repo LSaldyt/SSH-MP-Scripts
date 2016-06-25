b83919de76615f8158e8912346470f5af7fb3b96
lucas <lucassaldyt@gmail.com>
2016-06-24 17:50:58 -0700

    Creates folders for each type of script

--- git/setup_git.sh
+++ git/setup_git.sh
@@ -3,5 +3,5 @@ scp sudo_git_setup.sh "$1:"
 ssh $1 -t 'sudo sh sudo_git_setup.sh'
 
 echo Starting another ssh to add public key to the git repo
-./setup_ssh.sh $1
+../ssh/setup_ssh.sh $1
 echo Setup Complete. Call ./setup_ssh.sh from each additional computer that needs access to the repo
