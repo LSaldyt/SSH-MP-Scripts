8ff8b9c4c68fc8ecf1c7d457fe768102fda91703
LSaldyt <lucassaldyt@yahoo.com>
2016-06-26 11:06:14 -0600

    Adds methods for distibuting tasks across servers
    Specifically, cross_ssh.py:
      usage: ./cross_ssh.py servers tasks.pkl
      where servers is a text file of all of the pre-configured ssh servers
      and tasks.pkl is a serialized list of futures

--- mp/cross_ssh.py
+++ mp/cross_ssh.py
@@ -1,14 +1,53 @@
 #!/usr/bin/python
+from mp import *
+
+import dill as pickle
+
 import subprocess
 import sys
+import os
 
 if __name__ == "__main__":
     with open(sys.argv[1], 'r') as sshservers:
         servernames = sshservers.read().splitlines()
 
+    server_dict = {}
+    total_cores = 0
+
     for name in servernames:
-         print(name)
-         subprocess.call(['scp', 'sleep.py',  '%s:' % name])
-         subprocess.call(['ssh', name, 'python sleep.py 1'])
-         subprocess.call(['scp', '%s:out' % name, '%s.out' % name ])
+         # Get number of processors on each server machine
+         cores = subprocess.call(['ssh', name, 'NPROC="$(nproc)"; exit "$NPROC"'])
+         print("%s has %s cores" % (name, cores))
+         total_cores += cores
+         server_dict[name] = cores
+
+    def _f(x, y=0):
+        return [x * 42 for i in range(1000)]
+
+    futures = [Future(_f, [10], {'y' : 0})]
+
+    print('Total Cores: %s' % total_cores)
+
+    def split_list(alist, wanted_parts=1):
+        length = len(alist)
+        return [ alist[i*length // wanted_parts: (i+1)*length // wanted_parts] 
+                 for i in range(wanted_parts) ]
+
+    split_futures = split_list(futures, wanted_parts=total_cores)
+    print(split_futures)
+    
+    for server in server_dict:
+        print('Sending tasks to %s' % server)
+        server_cores = server_dict[server]
+        futures_chunk=split_futures[:server_cores]
+        split_futures=split_futures[server_cores:]
+        print('Tasks for %s : %s' % (server, futures_chunk))
+        with open('%s_tasks.pkl' % server, 'wb') as taskfile:
+            pickle.dump(futures_chunk, taskfile)
+        subprocess.call(['scp', '%s_tasks.pkl' % server,  '%s:' % server])
+        os.remove('%s_tasks.pkl' % server)
+
+#    subprocess.call(['ssh', name, 'python sleep.py 1'])
+ #   subprocess.call(['scp', '%s:out' % name, '%s.out' % name ])
+   
 
